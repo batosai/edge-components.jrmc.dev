bba9add33c471fba13a456c6e29569595cdbb2b9
jeremy <jeremy@chaufourier.fr>
2024-03-27 23:40:31 +0100

    Init

---
+++ start/view.ts
@@ -0,0 +1,15 @@
+
+import edge from 'edge.js'
+import env from '#start/env'
+// import { edgePluginComponent } from '@jrmc/edge-components'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as tablerIcons } from '@iconify-json/tabler'
+import { icons as fluentIcons } from '@iconify-json/fluent'
+
+edge.global('env', env)
+
+addCollection(tablerIcons)
+addCollection(fluentIcons)
+
+edge.use(edgeIconify)
+// edge.use(edgePluginComponent)
