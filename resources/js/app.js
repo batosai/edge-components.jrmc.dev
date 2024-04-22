0a939de202d99c5dbc1c2972484bd04481838394
jeremy <jeremy@chaufourier.fr>
2024-04-22 23:07:58 +0200

    add: docsearch

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import 'unpoly/unpoly.css'
+import '@docsearch/css'
 import '../css/app.css'
 
 import 'unpoly'
@@ -6,6 +7,8 @@ import Alpine from 'alpinejs'
 
 import { themeChange } from 'theme-change'
 import ClipboardJS from 'clipboard'
+
+import docsearch from '@docsearch/js'
 
 themeChange()
 new ClipboardJS('.btn-clipboard')
@@ -23,3 +26,11 @@ up.layer.config.drawer.position = 'right'
 
 // Alpine
 Alpine.start()
+
+// docsearch
+docsearch({
+  container: '#docsearch',
+  appId: 'UMZPU4KZ1I',
+  indexName: 'edge-components',
+  apiKey: '97e7c2b7a7dc8498613878efa94d8625',
+});
