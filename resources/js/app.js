d8623d4ea96c8df15c836bfb5d908f6838f035fb
jeremy <jeremy@chaufourier.fr>
2025-02-10 23:58:58 +0100

    feat(beta): tailwindcss 4 & daisyui 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,8 +16,8 @@ new ClipboardJS('.btn-clipboard')
 // Unpoly
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
-up.feedback.config.currentClasses.push(['active'])
-up.feedback.config.navSelectors.push(['nav'])
+up.status.config.currentClasses.push(['menu-active'])
+up.status.config.navSelectors.push(['nav'])
 
 up.layer.config.drawer.size = 'large'
 up.layer.config.drawer.position = 'right'
